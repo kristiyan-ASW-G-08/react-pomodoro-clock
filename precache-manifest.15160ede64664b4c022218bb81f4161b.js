25a9ca4a3a083e2d34affea9bb9625e70a4decff
Kristiyan Todorov <kristiyantkirchev@gmail.com>
2019-05-26 18:05:39 +0300

    Updates

---
+++ precache-manifest.15160ede64664b4c022218bb81f4161b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c5ed2d197eb2ce7a555a5eb8e22048e",
+    "url": "/react-pomodoro-clock/index.html"
+  },
+  {
+    "revision": "08b1820172baa15aad95",
+    "url": "/react-pomodoro-clock/static/css/main.f80e0620.chunk.css"
+  },
+  {
+    "revision": "4c573984e6abd2787146",
+    "url": "/react-pomodoro-clock/static/js/2.9a6bbf22.chunk.js"
+  },
+  {
+    "revision": "08b1820172baa15aad95",
+    "url": "/react-pomodoro-clock/static/js/main.6a0c8c54.chunk.js"
+  },
+  {
+    "revision": "6719385d069ef5bb6fdf",
+    "url": "/react-pomodoro-clock/static/js/runtime~main.8f834abd.js"
+  }
+]);
